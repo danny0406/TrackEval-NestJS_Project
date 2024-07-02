1b877237bda731bc1efabbad7f3c781e085a3944
danny0406 <kevinpoma312@gmail.com>
2024-07-01 20:46:41 -0400

    ADD server postgres and node with docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      expandVariables: true, 
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
