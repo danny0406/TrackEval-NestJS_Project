1f603a0797d25c2382a8b5b2b5c0979b2da2fe5f
danny0406 <kevinpoma312@gmail.com>
2024-07-08 02:19:33 -0400

    ADD excludes colums user.entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,4 +1,6 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { Exclude } from 'class-transformer'
 
 @Entity()
 export class User {
@@ -12,15 +14,19 @@ export class User {
   @Column({ type: 'varchar', length: 30 })
   lastname: string;
 
+  @Exclude()
   @Column({ type: 'varchar', length: 15, unique: true })
   username: string;
 
+  @Exclude()
   @Column({ type: 'varchar', length: 40})
   email: string;
 
+  @Exclude()
   @Column({ type: 'varchar' })
   password: string;
 
+  @Exclude()
   @Column({ type: 'enum', enum: ['t', 's', 'g'] })
   /**
    * t - teacher
@@ -35,4 +41,10 @@ export class User {
    * f - feminine
    */
   gender: string;
+
+
+
+  async validatePassword(password: string) {
+    return bcrypt.compare(password, this.password)
+  }
 }
