8efe2dacf4c02f54699c7c2ef3a3f33603bbfac7
danny0406 <kevinpoma312@gmail.com>
2024-07-11 11:11:37 -0400

    ADD handle connect and disconnect

--- src/game/game.gateway.ts
+++ src/game/game.gateway.ts
@@ -5,19 +5,30 @@ import {
   SubscribeMessage,
   MessageBody,
   ConnectedSocket,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { GameService } from './game.service';
 
 @WebSocketGateway({ namespace: 'game' })
-export class GameGateway {
+export class GameGateway 
+implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
+
+  handleConnection(client: any, ...args: any[]) {
+    console.log('Client connect ',client.id)
+  }
+  handleDisconnect(client: any) {
+    console.log('Client disconnect ',client.id)
+  }
 
   constructor(private readonly gameService: GameService) {}
 
   @SubscribeMessage('joinGame')
   handleJoinGame(@MessageBody() data: { gameId: number }, @ConnectedSocket() client: Socket): void {
+    console.log(data)
     client.join(`game_${data.gameId}`);
     client.emit('joinedGame', { message: 'You have joined the game' });
   }
