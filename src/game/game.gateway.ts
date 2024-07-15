f1c33c2704920cdac6f153572494d2d21bf90a55
danny0406 <kevinpoma312@gmail.com>
2024-07-15 16:09:10 -0400

    ADD implements submit Answer

--- src/game/game.gateway.ts
+++ src/game/game.gateway.ts
@@ -6,21 +6,26 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-import { Logger } from '@nestjs/common';
+import { Logger, NotFoundException, Injectable } from '@nestjs/common';
 import { GameService } from './game.service';
+import { QuizService } from 'src/quiz/quiz.service';
 
 @WebSocketGateway({
   cors: {
     origin: 'http://localhost:5173', // Permitir solicitudes desde esta URL
     methods: ['GET', 'POST'],
-    credentials: true
-  }
+    credentials: true,
+  },
 })
+@Injectable()
 export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
   private logger: Logger = new Logger('GameGateway');
 
-  constructor(private readonly gamesService: GameService) {}
+  constructor(
+    private readonly gamesService: GameService,
+    private readonly quizService: QuizService,
+  ) {}
 
   handleConnection(client: Socket, ...args: any[]) {
     this.logger.log(`Client connected: ${client.id}`);
@@ -31,27 +36,92 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
   }
 
   @SubscribeMessage('joinGame')
-  async handleJoinGame(client: Socket, payload: { pin: string, nickname: string }) {
+  async handleJoinGame(
+    client: Socket,
+    payload: { pin: string; nickname: string },
+  ) {
+    try {
+      const game = await this.gamesService.findByPin(payload.pin);
 
-    const game = await this.gamesService.findByPin(payload.pin);
-
-    if (game) {
-      client.join(game.id.toString());
-      const player = await this.gamesService.addPlayer(game.id, payload.nickname);
-      this.server.to(game.id.toString()).emit('playerJoined', player);
-    } else {
-      client.emit('error', { message: 'Game not found' });
+      if (game) {
+        client.join(game.id.toString());
+        const player = await this.gamesService.addPlayer(
+          game.id,
+          payload.nickname,
+        );
+        this.server.to(game.id.toString()).emit('playerJoined', player);
+      } else {
+        client.emit('error', { message: 'Game not found' });
+      }
+    } catch (error) {
+      this.logger.error(`Error in handleJoinGame: ${error.message}`);
+      client.emit('error', { message: error.message });
     }
   }
 
   @SubscribeMessage('startGame')
   async handleStartGame(client: Socket, payload: { gameId: string }) {
-    await this.gamesService.startGame(payload.gameId);
-    this.server.to(payload.gameId).emit('gameStarted','Start the GAME!!!');
+    try {
+      const quiz = await this.quizService.findOne(parseInt(payload.gameId));
+      if (!quiz) {
+        throw new NotFoundException('Quiz not found');
+      }
+
+      // Empezar enviando la primera pregunta
+      if (quiz.questions.length > 0) {
+        this.sendQuestion(client, quiz.questions[0], payload.gameId);
+      }
+      this.server.to(payload.gameId).emit('gameStarted', 'Start the Game');
+    } catch (error) {
+      this.logger.error(`Error in handleStartGame: ${error.message}`);
+      client.emit('error', { message: error.message });
+    }
+  }
+
+  private sendQuestion(client: Socket, question: any, gameId: string) {
+    client.to(gameId).emit('newQuestion', question);
   }
 
   @SubscribeMessage('submitAnswer')
-  async handleSubmitAnswer(client: Socket, payload: { gameId: string, questionId: string, answer: string }) {
-    // Procesar la respuesta
+  async handleSubmitAnswer(
+    client: Socket,
+    payload: { gameId: string; questionId: string; answer: string },
+  ) {
+    const { gameId, questionId, answer } = payload;
+
+    try {
+      const quiz = await this.quizService.findOne(parseInt(gameId));
+
+      const question = quiz.questions.find((q) => q.id === parseInt(questionId));
+      if (!question) {
+        throw new NotFoundException('Question not found');
+      }
+
+      const selectedAnswer = question.answers.find((a) => a.text === answer);
+      if (!selectedAnswer) {
+        throw new NotFoundException('Answer not found');
+      }
+
+      const isCorrect = selectedAnswer.isCorrect;
+      client.emit('answerResult', { success: true, isCorrect });
+
+      // Emitir a todos los jugadores que esta pregunta ha sido respondida
+      this.server.to(gameId).emit('answerSubmitted', { questionId, isCorrect });
+
+      // Encontrar el índice de la pregunta actual y enviar la siguiente si existe
+      const currentQuestionIndex = quiz.questions.findIndex(q => q.id === parseInt(questionId));
+      if (currentQuestionIndex < quiz.questions.length - 1) {
+        this.sendQuestion(client, quiz.questions[currentQuestionIndex + 1], gameId);
+      } else {
+        // Si no hay más preguntas, finalizar el juego
+        this.server.to(gameId).emit('gameEnded', 'Game has ended');
+      }
+    } catch (error) {
+      this.logger.error(`Error in handleSubmitAnswer: ${error.message}`);
+      client.emit('answerResult', {
+        success: false,
+        message: error.message,
+      });
+    }
   }
 }
--- src/game/game.module.ts
+++ src/game/game.module.ts
@@ -5,9 +5,10 @@ import { Player } from './entities/player.entity';
 import { GameService } from './game.service';
 import { GameController } from './game.controller';
 import { GameGateway } from './game.gateway';
+import { QuizModule } from 'src/quiz/quiz.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Game, Player])],
+  imports: [TypeOrmModule.forFeature([Game, Player]),QuizModule],
   providers: [GameService, GameGateway],
   controllers: [GameController],
 })
