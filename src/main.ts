4e34fffc7729591f57ddf6a2fe018ec34dd4e00b
danny0406 <kevinpoma312@gmail.com>
2024-07-15 15:34:31 -0400

    Fix, cors activate

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(new ValidationPipe(
     {
