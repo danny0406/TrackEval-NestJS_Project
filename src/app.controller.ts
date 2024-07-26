d3249667caeaf94194853b9c27c0212ec0fdcbc1
danny0406 <kevinpoma312@gmail.com>
2024-07-26 16:55:30 -0400

    ADD swagger: Tags and operation.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Public } from './decorators/public.decorator';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Hello World')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Public()
   @Get()
+  @ApiOperation({ summary: 'Get - Hello world' })
   getHello(): string {
     return this.appService.getHello();
   }
