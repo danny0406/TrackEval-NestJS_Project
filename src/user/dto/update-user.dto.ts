8793172e0ea849258cfe5f0c9465790cec9db00b
danny0406 <kevinpoma312@gmail.com>
2024-07-26 16:52:25 -0400

    ADD Users-swagger: opertions, responses ,property and propertyOptional

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,28 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { Role } from 'src/enums/role.enum';
+import { Gender } from 'src/enums/gender.enum';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    @ApiPropertyOptional()
+    name?: string;
+
+    @ApiPropertyOptional()
+    lastname?: string;
+
+    @ApiPropertyOptional()
+    username?: string;
+
+    @ApiPropertyOptional()
+    email?: string;
+
+    @ApiPropertyOptional()
+    password?: string;
+
+    @ApiPropertyOptional({ enum: Role })
+    usertype?: Role;
+
+    @ApiPropertyOptional({ enum: Gender})
+    gender?: Gender;
+}
