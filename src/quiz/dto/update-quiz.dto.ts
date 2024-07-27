1b43e0ba2e62e1cd8f3833e5d880a6ac7c09799c
danny0406 <kevinpoma312@gmail.com>
2024-07-26 20:34:25 -0400

    ADD Quiz-Swagger

--- src/quiz/dto/update-quiz.dto.ts
+++ src/quiz/dto/update-quiz.dto.ts
@@ -1,8 +1,10 @@
 import { ArrayMinSize, IsOptional, IsString, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 import { UpdateQuestionDto } from 'src/question/dto/update-question.dto';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class UpdateQuizDto {
+    @ApiPropertyOptional()
     @IsString()
     @IsOptional()
     title?: string;
